e4d8b2bd43012d211096a323cf7b26ea6ff2028a
fienleeflang <fien.leeflang@gmail.com>
2020-01-20 10:10:23 +0100

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,7 +1,30 @@
-$( "#thumbnail" ).click(function() {
-  $(this).toggleClass('large')
+$( "#left" ).click (function() {
+  $(this).toggleClass('.large')
+});
+
+$( "#right" ).click(function() {
+  $(this).toggleClass('.large')
 });
 
 $( ".container img" ).click(function() {
-  $(this).toggleClass('large')
+  $(this).toggleClass('.large')
 });
+
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
